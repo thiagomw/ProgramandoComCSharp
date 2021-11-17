0fa6963b08afd374e49d0d225b087cf5e69ca265
Thiago Ribeiro <thiagoribeiromw@gmail.com>
2021-11-16 21:39:09 -0300

    Aba Clientes - inclusão de Cliente

--- Fintech.Correntista.Wpf/MainWindow.xaml.cs
+++ Fintech.Correntista.Wpf/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Fintech.Dominio.Entidades;
 
 namespace Fintech.Correntista.Wpf
 {
@@ -20,14 +10,59 @@ namespace Fintech.Correntista.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Cliente> clientes = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            PopularControles();
+        }
+
+        private void PopularControles()
+        {
+            sexoComboBox.Items.Add(Sexo.Feminino);
+            sexoComboBox.Items.Add(Sexo.Masculino);
+            sexoComboBox.Items.Add(Sexo.Outros);
+
+            clienteDataGrid.ItemsSource = clientes;
         }
 
         private void incluirClienteButton_Click(object sender, RoutedEventArgs e)
         {
+            //Cliente cliente = new();
+            var cliente = new Cliente();
 
+            cliente.Cpf = cpfTextBox.Text;
+            cliente.DataNascimento = Convert.ToDateTime(dataNascimentoTextBox.Text);
+
+            var endereco = new Endereco();
+            endereco.Cep = cepTextBox.Text;
+            endereco.Cidade = cidadeTextBox.Text;
+            endereco.Logradouro = logradouroTextBox.Text;
+            endereco.Numero = numeroLogradouroTextBox.Text;
+
+            cliente.EnderecoResidencial = endereco;
+
+            cliente.Nome = nomeTextBox.Text;
+            cliente.Sexo = (Sexo)sexoComboBox.SelectedItem;
+
+            clientes.Add(cliente);
+
+            MessageBox.Show("Cliente cadastrado com sucesso!");
+            LimparControles();
+            pesquisaClienteTabItem.Focus();
+            clienteDataGrid.Items.Refresh();
+        }
+        private void LimparControles()
+        {
+            cpfTextBox.Clear();
+            nomeTextBox.Text = "";
+            dataNascimentoTextBox.Text = null;
+            sexoComboBox.SelectedIndex = -1;
+            logradouroTextBox.Text = string.Empty;
+            cepTextBox.Clear();
+            cidadeTextBox.Clear();
+            numeroLogradouroTextBox.Clear();
         }
     }
 }
