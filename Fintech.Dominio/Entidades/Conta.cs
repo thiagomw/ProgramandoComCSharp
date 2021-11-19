4fa106e2d5ddaeeecba0b63048bd54ef9f82721a
Thiago Ribeiro <thiagoribeiromw@gmail.com>
2021-11-18 22:56:41 -0300

    Criação da classe Movimento

--- Fintech.Dominio/Entidades/Conta.cs
+++ Fintech.Dominio/Entidades/Conta.cs
@@ -18,9 +18,12 @@ namespace Fintech.Dominio.Entidades
         public string DigitoVerificador { get; set; }
         public decimal Saldo { get; set; }
         public Cliente Cliente { get; set; }
+        public List<Movimento> Movimentos { get; set; } = new List<Movimento>();
 
         public virtual void EfetuarOperacao(decimal valor, Operacao operacao)
         {
+            var sucesso = true;
+            
             switch (operacao)
             {
                 case Operacao.Deposito:
@@ -31,7 +34,16 @@ namespace Fintech.Dominio.Entidades
                     {
                         Saldo -= valor; 
                     }
+                    else
+                    {
+                        sucesso = false;
+                    }
                     break;
+            }
+
+            if (sucesso)
+            {
+                Movimentos.Add(new Movimento(operacao, valor));
             }
         }
 
---
+++ Fintech.Dominio/Entidades/Movimento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fintech.Dominio.Entidades
+{
+    public class Movimento
+    {
+        public Movimento(Operacao operacao, decimal valor)
+        {
+            Operacao = operacao;
+            Valor = valor;
+        }
+
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public DateTime DataTransacao { get; set; } = DateTime.Now;
+        public Operacao Operacao { get; set; }
+        public decimal Valor { get; set; }
+        public Conta Conta { get; set; }
+    }
+}
